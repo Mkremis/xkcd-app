9dc5f59492fb043d873d2e88481841891f0cbeb7
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-22 20:34:48 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,20 @@ export default function Home({lastestComics}) {
       </Head>
       <Header />
       <main>
-        <h2>Latest Comics</h2>
-    {lastestComics.map(comic=>{
+        <h2 className='text-3xl font-bold text-center mb-10'>Latest Comics</h2>
+        <section className='grid grid-cols-1 gap-2 max-w-md m-auto sm:grid-cols-2'>
+        {lastestComics.map(comic=>{
       return(
           <Link key={comic.id} href={`/comic/${comic.id}`}>
-          <Image layout="fill" src={comic.img} alt={comic.alt}/>
+            <div className='mb-4 pb-4 m-auto'>
+            <h3 className='font-bold text-sm text-center pb-2'>{comic.title}</h3>
+          <Image width='300' height='300' layout='intrinsic' objectFit='contain' src={comic.img} alt={comic.alt}/>
+          </div>
           </Link>
+      
       )
     })}
+        </section>
       </main>
     </div>
   );
