2b1d907e3eb58fdfa9ac52bacfce81b38bafd6f6
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-22 18:52:22 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import fs from "fs/promises"
+import { isUtf8 } from 'buffer';
 
-export default function Home() {
+export default function Home({lastestComics}) {
   return (
     <div>
       <Head>
@@ -13,4 +15,17 @@ export default function Home() {
     </div>
   );
 }
-export async function getStaticProps(context) {}
+export async function getStaticProps(context) {
+ const files = await fs.readdir('./comics');
+ const lastestComicsFiles = files.slice(-8, files.length);
+ const promisesReadFiles = lastestComicsFiles.map(async(file)=>{
+  const content = await fs.readFile(`./comics/${file}`, 'utf-8');
+  return{content}
+ })
+const lastestComics = await Promise.all(promisesReadFiles)
+  return{
+    props:{
+      lastestComics
+    }
+  }
+}
